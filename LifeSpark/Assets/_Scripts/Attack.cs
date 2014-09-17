4d26ad43143d7ca56ef5cfc93cd731aaa67dc1d2
Boston Mak <boston@makotexas.com>
2014-09-17 00:49:57 -0700

    preliminary attack/hit objects

---
+++ LifeSpark/Assets/_Scripts/Attack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+    float attackRange;
+    float attackRadius;
+    float attackDuration;
+    float remainingDuration;
+    float speed;
+    Player source;
+    Vector3 target;
+
+    public enum AttackType {
+        Line,
+        TargetArea,
+        SelfArea
+    };
+
+    public AttackType attackType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Attack() {
+
+    }
+}
